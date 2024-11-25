33bd52b83581bc3dd965eba0a6b7ea6b2507446a
YongHyun-Yi <dldydgus1995@naver.com>
2024-11-25 16:14:29 +0900

    feat: add other docu's scripts

---
+++ blue_docu/blue_docu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func normal_press() -> void:
+	print("Wrong!")
+	pass
+
+func double_press() -> void:
+	print("Right!")
+	pass
+
+func long_press() -> void:
+	print("Wrong!")
+	pass
