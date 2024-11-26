e0e99dae2436bc0785c1253399b4139ed53fb6f6
YongHyun-Yi <dldydgus1995@naver.com>
2024-11-26 16:54:47 +0900

    style: change docu to paper

--- main.gd
+++ main.gd
@@ -1,37 +1,39 @@
 extends Node2D
 
-var docu_timer_rng = RandomNumberGenerator.new()
-var docu_type_rng = RandomNumberGenerator.new()
+var PaperTimerRng = RandomNumberGenerator.new()
+var PaperTypeRng = RandomNumberGenerator.new()
 
-@onready var docu_timer = get_node("Timer")
-@export var docu_types : Array[PackedScene]
-var docu_arr: Array
+@onready var PaperTimer = get_node("Timer")
+@export var PaperTypes : Array[PackedScene]
+var PaperArr: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	docu_timer_rng.randomize()
-	docu_type_rng.randomize()
-	docu_timer.wait_time = docu_timer_rng.randf_range(0.7, 1.2)
+	PaperTimerRng.randomize()
+	PaperTypeRng.randomize()
+	PaperTimer.wait_time = PaperTimerRng.randf_range(0.7, 1.2)
 	for i in 5:
-		var docu_type = docu_type_rng.randi_range(0, 2)
-		var docu_ins = docu_types[docu_type].instantiate()
-		docu_arr.push_back(docu_ins)
+		var PaperType = PaperTypeRng.randi_range(0, 2)
+		PaperType = 0 # test
+		var PaperIns = PaperTypes[PaperType].instantiate()
+		PaperArr.push_back(PaperIns)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var docu_arr_items: String
-	for i in docu_arr.size():
-		docu_arr_items += docu_arr[i].name + " "
-	$docu_arr.text = docu_arr_items
+	var PaperArrItems: String
+	for i in PaperArr.size():
+		PaperArrItems += PaperArr[i].name + " "
+	$PaperArr.text = PaperArrItems
 	pass
 
 
 func _on_timer_timeout() -> void:
-	docu_timer.wait_time = docu_timer_rng.randf_range(0.7, 1.2)
-	var tmp_docu = docu_arr.pop_front()
-	$DocuHolder.add_child(tmp_docu)
-	var docu_type = docu_type_rng.randi_range(0, 2)
-	var docu_ins = docu_types[docu_type].instantiate()
-	docu_arr.push_back(docu_ins)
+	PaperTimer.wait_time = PaperTimerRng.randf_range(0.7, 1.2)
+	var TmpPaper = PaperArr.pop_front()
+	$PaperHolder.add_child(TmpPaper)
+	var PaperType = PaperTypeRng.randi_range(0, 2)
+	PaperType = 0 # test
+	var PaperIns = PaperTypes[PaperType].instantiate()
+	PaperArr.push_back(PaperIns)
--- scissors/scissors.gd
+++ scissors/scissors.gd
@@ -30,17 +30,17 @@ func long_pressed() -> void:
 	emit_signal("long_press")
 	pass
 
-# area에 들어온 문서 하나만 추적한다
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	# 이전에 추적중인 문서가 있다면 signal을 모두 disconnect 한다
+# area에 들어온 종 하나만 추적한다
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	# 이전에 추적중인 종가 있다면 signal을 모두 disconnect 한다
 	if target != null:
 		normal_press.disconnect(target.normal_press)
 		double_press.disconnect(target.double_press)
 		long_press.disconnect(target.long_press)
 	
 	# 새 목표 문서에 signal을 connect
-	target = area.get_parent()
-	normal_press.connect(target.normal_press)
+	target = body
+	normal_press.connect(target.nor mal_press)
 	double_press.connect(target.double_press)
 	long_press.connect(target.long_press)
 	pass # Replace with function body.
