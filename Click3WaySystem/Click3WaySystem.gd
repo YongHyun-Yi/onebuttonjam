ce028a75f30ff69bc62aeba288c7a41f66ac7ccc
YongHyun-Yi <dldydgus1995@naver.com>
2024-11-27 18:50:08 +0900

    fix: copy paste gpt's code to implement 3wayControl

--- Click3WaySystem/Click3WaySystem.gd
+++ Click3WaySystem/Click3WaySystem.gd
@@ -1,85 +1,64 @@
 extends Node
 
-var long_pressed = false
-var normal_pressed = false
-
-var once_pressed = false
-var press_time = 0.0
-var double_press_time = 0.02
-var long_press_time = 0.3
+# Constants for detection thresholds
+const DOUBLE_TAP_TIME = 0.17  # Maximum interval for double-tap detection
+const LONG_TAP_TIME = 0.5   # Minimum hold time for long tap
 
 signal normal_press
 signal double_press
 signal long_press
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Variables to track touch states
+var tap_start_time = 0.0
+var last_tap_time = -1.0
+var is_tapping = false
+var is_double_tap = false
+var long_tap_triggered = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-# 도장 입력시스템 구현
-# 시간에 상관없이 한번만 누를경우 한 번 누르기
-# 일정시간 이상 눌렀다가 떼면 꾹 누르기
-# 짧은 시간에 두 번 누를경우 두 번 누르기
-# 한 번 누르고나서 짧은 시간안에 다시 누르면 두 번 누르기
-# 꾹 누르기용 타이머, 두 번 누르기용 타이머 따로 만들기
-# 한 번 누르기와 두 번 누르기를 어떻게 구분하는지?
-func _process(delta: float) -> void:
-	$CanvasLayer/ProgressBar3.value = press_time
-	if press_time != 0.0:
-		press_time += delta
-	
-	if press_time >= double_press_time:
-		once_pressed = false
-		#print("normal press!!")
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		$long_press.start()
-		if once_pressed == true:
-			if press_time < double_press_time:
-				once_pressed = false
-				press_time = 0
-				print("double press!!")
+func _process(delta):
+	# Handle touch input using actions
+	if Input.is_action_pressed("ui_accept"):
+		if not is_tapping:
+			# Start of a new tap
+			is_tapping = true
+			tap_start_time = Time.get_ticks_msec() / 1000.0
+			long_tap_triggered = false
 		else:
-			once_pressed = true
-			press_time = delta
-			#print("normal press!!")
-	
-	
-	
-	if Input.is_action_just_released("ui_accept"):
-		#if long_pressed == true:
-			#emit_signal("long_press")
-			#print("Long Press!")
-			#long_pressed = false
-		#else:
-			#$long_press.stop()
-			#if normal_pressed == false:
-				#normal_pressed = true
-				#$double_press.start()
-			#else:
-				#emit_signal("double_press")
-				#print("Double Press!")
-				#normal_pressed = false
-				#$double_press.stop()
-		if press_time >= long_press_time:
-			print("long press!!")
-			press_time = 0
-	
-	$CanvasLayer/ProgressBar.value = $long_press.time_left
-	$CanvasLayer/ProgressBar2.value = $double_press.time_left
-	pass
+			# Check for long tap only if the touch is held
+			var current_time = Time.get_ticks_msec() / 1000.0
+			if (current_time - tap_start_time) >= LONG_TAP_TIME:
+				long_tap_triggered = true  # Mark long tap as triggered
+	elif Input.is_action_just_released("ui_accept"):
+		# End of touch: determine the type of tap
+		if is_tapping:
+			var tap_duration = Time.get_ticks_msec() / 1000.0 - tap_start_time
+			is_tapping = false
+			
+			if long_tap_triggered:
+				# If it was a long tap, trigger the long tap handler
+				emit_signal("long_press")
+				print("long tap")
+			else:
+				# Otherwise, check for single or double tap
+				_handle_tap()
 
+	# Reset double-tap state after DOUBLE_TAP_TIME
+	if last_tap_time > 0.0 and (Time.get_ticks_msec() / 1000.0 - last_tap_time) > DOUBLE_TAP_TIME:
+		is_double_tap = false
 
-func _on_long_press_timeout() -> void:
-	long_pressed = true
-	pass # Replace with function body.
-
-
-func _on_double_press_timeout() -> void:
-	emit_signal("normal_press")
-	#print("Normal Press!")
-	normal_pressed = false
-	pass # Replace with function body.
+func _handle_tap():
+	var current_time = Time.get_ticks_msec() / 1000.0
+	if last_tap_time > 0.0 and (current_time - last_tap_time) <= DOUBLE_TAP_TIME:
+		# Double tap detected
+		emit_signal("double_press")
+		print("double tap")
+		last_tap_time = -1.0  # Reset last tap time
+		is_double_tap = true
+	else:
+		# Schedule single tap to be triggered after DOUBLE_TAP_TIME
+		is_double_tap = false
+		last_tap_time = current_time
+		await get_tree().create_timer(DOUBLE_TAP_TIME).timeout
+		if not is_double_tap:
+			emit_signal("normal_press")
+			print("single tap")
