7fbf6c082d33cf021fd76e18afea3ed1f11d4e5d
YongHyun-Yi <dldydgus1995@naver.com>
2024-12-03 16:52:34 +0900

    feat: credit

--- title/title_ui.gd
+++ title/title_ui.gd
@@ -14,3 +14,13 @@ func _process(delta: float) -> void:
 func _on_start_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
 	pass # Replace with function body.
+
+
+func _on_credit_button_button_up() -> void:
+	$Credit.show()
+	pass # Replace with function body.
+
+
+func _on_credit_close_button_button_up() -> void:
+	$Credit.hide()
+	pass # Replace with function body.
