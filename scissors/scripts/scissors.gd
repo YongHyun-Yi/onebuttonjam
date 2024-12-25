4e932aa834c9a5489aca47891dd21ef540bfa709
YongHyun-Yi <dldydgus1995@naver.com>
2024-12-25 23:51:14 +0900

    remake scissors movement - simple touch move

--- scissors/scripts/scissors.gd
+++ scissors/scripts/scissors.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 
-#var isMove: bool = false
+var isMove: bool = false
 var direction: Vector2 = Vector2.ZERO
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var targetPosition: Vector2 = Vector2.ZERO
+const SPEED = 1200.0
+const GRAVITY_MAX = 130.0
 
 
 func _input(event):
 	if event is InputEventScreenTouch and event.pressed:
+		isMove = true
+		targetPosition = event.position
 		direction = global_position.direction_to(event.position).normalized()
-		print("hi")
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#if not isMove:
-		#velocity += get_gravity() * delta
-	velocity = direction * SPEED
+	if isMove:
+		velocity = direction * SPEED
+		if abs(global_position.distance_to(targetPosition)) < 10:
+			isMove = false
+			velocity = Vector2.ZERO
+	else:
+		velocity += get_gravity() * delta
+		if velocity.y > GRAVITY_MAX:
+			velocity.y = GRAVITY_MAX
 
 	move_and_slide()
